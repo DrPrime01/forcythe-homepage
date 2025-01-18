7257a751275a0dbe78748bf7f06f17835a1a1358
DrPrime01 <drprime.dev@gmail.com>
2025-01-18 23:22:48 +0100

    feat: updated fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,31 @@
 import scrollbarHide from "tailwind-scrollbar-hide";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        secondary: "#064386",
-        accent: "#60a6e7",
-        accent2: "#b3d0f2",
-        accent3: "#0c2645",
-        "dark-grey": "#aea9b1",
-        "light-grey": "#e6e0e9",
-        bg: {
-          primary: "#030516",
-          secondary: "#1f3449",
-        },
-      },
-      spacing: {
-        4.5: "18px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			secondary: '#064386',
+  			accent: '#60a6e7',
+  			accent2: '#b3d0f2',
+  			accent3: '#0c2645',
+  			'dark-grey': '#aea9b1',
+  			'light-grey': '#e6e0e9',
+  			bg: {
+  				primary: '#030516',
+  				secondary: '#1f3449'
+  			}
+  		},
+  		spacing: {
+  			'4.5': '18px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [scrollbarHide],
+  plugins: [scrollbarHide, require("tailwindcss-animate")],
 };
